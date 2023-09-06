cb0335efa41857a0249fcbddddf7ddcadcaa7be9
Isaías Ramos <isaias.ramos.olive@gmail.com>
2023-09-06 16:06:48 -0300

    refactor: global style

--- src/app/components/ToggleTheme/index.tsx
+++ src/app/components/ToggleTheme/index.tsx
@@ -6,7 +6,10 @@ const ToggleTheme = () => {
     globalThis.document?.documentElement?.classList.toggle("dark");
   };
   return (
-    <a className="itemAside bg-red-500 dark:bg-primary-400 w-[64px] absolute sm:relative top-[54px] sm:top-auto">
+    <a
+      className="itemAside dark:hover:bg-primary-200
+            hover:bg-red-400 bg-red-500 dark:bg-primary-400 w-[64px] absolute sm:relative top-[54px] sm:top-auto"
+    >
       <MoonIcon className="icon block dark:hidden" onClick={toggle} />
       <SunIcon className="icon hidden dark:block" onClick={toggle} />
     </a>
